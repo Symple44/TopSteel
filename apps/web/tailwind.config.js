467d16313935556302f9d0a960bdc7379a5ff850
SYMPLE <nicolas.dubain@gmail.com>
2025-06-28 13:02:04 +0200

    Update tailwind.config.js

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,2 +1,2 @@
-// apps/web/tailwind.config.js - Configuration simplifiée utilisant les packages
+// apps/web/tailwind.config.js - Configuration utilisant les packages
 module.exports = require('../../packages/config/tailwind/web.cjs')
