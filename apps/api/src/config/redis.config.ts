874dfd4283f6da12fdc1a0a0ef49232e275645bf
SYMPLE <nicolas.dubain@gmail.com>
2025-06-30 12:46:52 +0200

    .

--- apps/api/src/config/redis.config.ts
+++ apps/api/src/config/redis.config.ts
@@ -7,4 +7,7 @@ export default registerAs('redis', () => ({
   db: parseInt(process.env.REDIS_DB ?? '0', 10) ,
   ttl: parseInt(process.env.REDIS_TTL ?? '3600', 10) ,
   max: parseInt(process.env.REDIS_MAX ?? '100', 10) ,
+  retryDelayOnFailover: 100,
+  maxRetriesPerRequest: 3,
+  lazyConnect: true,
 }));
